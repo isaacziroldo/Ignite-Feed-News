097e7d0543efe9bcacb03edbe2c53d13a61350aa
Isaac Ziroldo <50035007+isaacziroldo@users.noreply.github.com>
2023-01-29 19:29:59 -0300

    Sidebar Component Finished

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,13 @@
 
 import styles from './Sidebar.module.css'
+import { PencilLine } from "phosphor-react";
 
 export function Sidebar(){
     return(
         <aside className={styles.sidebar}>
             <img className={styles.cover} src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" alt="" />
             <div className={styles.profile}>
-
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/50035007?v=4" alt="" />
                 <strong>Isaac Ziroldo</strong>
                 <span>Web Developer</span>
             </div>
@@ -14,6 +15,7 @@ export function Sidebar(){
             <footer>
 
                 <a href="#">
+                    <PencilLine size={20}/>
                     Editar seu perfil
                 </a>
 
