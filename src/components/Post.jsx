f2a17f04e7eb2808c50385e4ec07f48c1d31b262
isaacziroldo <50035007+isaacziroldo@users.noreply.github.com>
2023-01-31 22:02:51 -0300

    Comment form

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -31,6 +31,20 @@ export function Post() {
                     <p> <a href="#"> #novoprojeto</a> <a href="#"> #nlw</a> <a href="#"> #rocketseat</a></p>
                 
             </div>
+            <form className={styles.commentForm}>
+                <strong>
+                    Deixe o seu feedback
+                </strong>
+                <textarea 
+                    placeholder='Deixe um comentário'
+                />
+            
+            <footer>
+                <button type='submit'>Publicar</button>
+
+            </footer>
+
+            </form>
         </article>
     )
 }
