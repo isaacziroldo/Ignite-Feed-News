319555b98c8fc51bd7359e919a6a0e47dfcd029f
isaacziroldo <50035007+isaacziroldo@users.noreply.github.com>
2023-02-01 22:06:38 -0300

    Avatar Component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
@@ -7,7 +8,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/50035007?v=4" alt="" />
+                    <Avatar src="https://avatars.githubusercontent.com/u/50035007?v=4" alt="" />
                     <div className={styles.authorInfo}>
 
                         <strong>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,17 @@
 
 import styles from './Sidebar.module.css'
 import { PencilLine } from "phosphor-react";
+import { Avatar } from './Avatar';
 
 export function Sidebar(){
     return(
         <aside className={styles.sidebar}>
             <img className={styles.cover} src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" alt="" />
+            
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/50035007?v=4" alt="" />
+                <Avatar
+                   src="https://avatars.githubusercontent.com/u/50035007?v=4"            
+                />
                 <strong>Isaac Ziroldo</strong>
                 <span>Web Developer</span>
             </div>
